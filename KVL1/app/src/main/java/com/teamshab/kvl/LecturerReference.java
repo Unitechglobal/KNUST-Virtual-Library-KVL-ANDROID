fc8d3e5a8183605b6cebed2eba6f2da21b920b3c
shaban00 <shaban00hassan@gmail.com>
2018-04-03 06:57:33 +0000

    LecturerReference modified

--- KVL1/app/src/main/java/com/teamshab/kvl/LecturerReference.java
+++ KVL1/app/src/main/java/com/teamshab/kvl/LecturerReference.java
@@ -1,30 +1,4 @@
-package com.teamshab.kvl;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-public class LecturerReference extends AppCompatActivity {
-
-    Button mReferenceButton;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lecturer_reference);
-
-        mReferenceButton = (Button) findViewById(R.id.lecturer_reference_button);
-        mReferenceButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(LecturerReference.this, LecturerHomeActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
 package com.teamshab.kvl;
 
 import android.content.Intent;
