a113868895d6449339cfd90eec19bdda473b04da
shaban00 <shaban00hassan@gmail.com>
2018-04-03 06:34:17 +0000

    NonStudentRegister modified

--- KVL1/app/src/main/java/com/teamshab/kvl/NonStudentRegister.java
+++ KVL1/app/src/main/java/com/teamshab/kvl/NonStudentRegister.java
@@ -10,4 +10,28 @@ public class NonStudentRegister extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_non_student_register);
     }
+package com.teamshab.kvl;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class NonStudentRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_non_student_register);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        Intent intent = new Intent(NonStudentRegister.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
 }
