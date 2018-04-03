0fcf5ae2e71c9e7deac871d8494a246eb6cb2f83
shaban00 <shaban00hassan@gmail.com>
2018-04-03 06:29:38 +0000

    LibrarianReference modified

--- KVL1/app/src/main/java/com/teamshab/kvl/LibrarianReference.java
+++ KVL1/app/src/main/java/com/teamshab/kvl/LibrarianReference.java
@@ -25,4 +25,32 @@ public class LibrarianReference extends AppCompatActivity {
             }
         });
     }
+package com.teamshab.kvl;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LibrarianReference extends AppCompatActivity {
+
+    Button mReferenceButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_librarian_reference);
+
+        mReferenceButton = (Button) findViewById(R.id.libarian_reference_button);
+        mReferenceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LibrarianReference.this, LibrarianHomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
 }
