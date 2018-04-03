1f02f0c36295eaacf701d1bd2c03a5757751140c
shaban00 <shaban00hassan@gmail.com>
2018-04-03 07:04:46 +0000

    StudentReference modified

--- KVL1/app/src/main/java/com/teamshab/kvl/StudentReference.java
+++ KVL1/app/src/main/java/com/teamshab/kvl/StudentReference.java
@@ -1,30 +1,4 @@
-package com.teamshab.kvl;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-public class StudentReference extends AppCompatActivity {
-
-    Button mReferenceButton;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_reference);
-
-        mReferenceButton = (Button) findViewById(R.id.student_reference_button);
-        mReferenceButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(StudentReference.this, StudentHomeActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
 package com.teamshab.kvl;
 
 import android.content.Intent;
